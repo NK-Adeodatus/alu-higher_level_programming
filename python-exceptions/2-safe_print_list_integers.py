85fdf214658d37838dc8d04478fede457f11867f
NK-Adeodatus <a.nkundiman@alustudent.com>
2024-10-28 04:06:37 -0700

    added file 2-safeprint

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,13 +3,11 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    try:
         for i in range(x):
+            try:
             if isinstance(my_list[i], int):
                 print("{:d}".format(my_list[i]), end="")
                 count += 1
-    except IndexError:
-        pass
-    finally:
-        print("")
+            except IndexError:
+                break
     return count

476942f865bd983ab276661c85fd33dc1dbad058
NK-Adeodatus <a.nkundiman@alustudent.com>
2024-10-28 23:47:35 -0700

    added a file 1-square

---
+++ python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module 1-square Defines a square based on module 0-square."""
+
+
+class Square:
+    """Empty class that defines a square"""
+    def __init__(self, size=0):
+        """Instantiate with size duriing object initialization"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size

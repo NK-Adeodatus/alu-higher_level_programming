18939ff4f29569940387bc94d9fab6a0cd6f6255
NK-Adeodatus <a.nkundiman@alustudent.com>
2024-10-29 01:24:45 -0700

    added a file 5-square

---
+++ python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Module 4-square - Defines a square with
+size property and area calculation."""
+
+
+class Square:
+    """Class that defines a square with a size property."""
+
+    def __init__(self, size=0):
+        """Initialize the square with an optional size."""
+        self.size = size  # Use the setter to initialize size
+
+    @property
+    def size(self):
+        """Retrieve the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square with type and value checks."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Calculate and return the area of the square."""
+        return self.__size **
+    def my_print(self):
+        """Print the square with character '#'"""
+        if self.__size == 0:
+            print("")
+        else:
+            for _ in range(self.__size):
+                print('#' * self.__size)

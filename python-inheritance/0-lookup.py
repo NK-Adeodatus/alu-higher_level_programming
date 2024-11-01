9e2d9a62d73ecac06a5268675bd05f1e81a5e581
NK-Adeodatus <a.nkundiman@alustudent.com>
2024-11-01 04:07:06 -0700

    corrections

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-
+"""returning the list of available attributes and methodes"""
 
 def lookup(obj):
-    dir(obj)
+    """function to return the attributes"""
+    return dir(obj)

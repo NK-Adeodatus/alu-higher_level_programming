9369672258368ff94eff35e4825fe4f30e1f73f8
NK-Adeodatus <a.nkundiman@alustudent.com>
2024-11-02 07:51:45 -0700

    added file 3 is kind of class

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A function for checking if obj is instance
+of a class or the subclass of a_class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Return true if obj is instance of a_class
+    or it's subclass"""
+    return is instance(obj, a_class)

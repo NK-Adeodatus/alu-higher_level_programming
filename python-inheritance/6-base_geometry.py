c5fd0bd16bb91bc50197d56152654f710cd1152e
NK-Adeodatus <a.nkundiman@alustudent.com>
2024-11-03 00:30:55 -0700

    added file 6-base geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """An empty class."""
+    def area(self):
+        """Raises an exception"""
+        raise Exception("area() is not implemented")

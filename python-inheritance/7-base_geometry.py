65c25109f05fd996bd0bf78da5edcbf8baa6e8f1
NK-Adeodatus <a.nkundiman@alustudent.com>
2024-11-03 00:46:00 -0700

    added file 7 base geometry

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """An empty class."""
+    def area(self):
+        """Raises an exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validate the value"""
+        if isinstance(value, int):
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")

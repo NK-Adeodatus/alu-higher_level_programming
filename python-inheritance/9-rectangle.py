c1c6d81b42d6085c693102c56e608887bfd26b7b
NK-Adeodatus <a.nkundiman@alustudent.com>
2024-11-09 00:04:10 -0800

    added file 9rectangle

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
-"""
-Defines a class Rectangle that inherits
-from BaseGeometry.
-"""
+"""Defines a class Rectangle that inherits from BaseGeometry."""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """
-    A class to represent rectangle using baseGeometry.
-    """
+    """A class to represent a rectangle using BaseGeometry."""
 
     def __init__(self, width, height):
-        """Initialize a new rectangle instance."""
-        #Validate width and height using integer_validator method
+        """Initialize a new Rectangle instance.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
+        # Validate width and height using the integer_validator method
         self.integer_validator("width", width)
         self.integer_validator("height", height)
 
-        #set private attributes for width and height
+        # Set private attributes for width and height
         self.__width = width
-        self.__height =height
+        self.__height = height
 
     def area(self):
         """Calculate the area of the rectangle."""

d572322a58e8e8ab6e62908876d395a9abd6dbb8
NK-Adeodatus <a.nkundiman@alustudent.com>
2024-11-11 03:55:43 -0800

    added 8-class_to_json.py

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module contains the function `class_to_json`,
+which takes an object
+and returns its dictionary representation for JSON serialization.
+"""
+
+def class_to_json(obj):
+    """
+    Returns the dictionary description of an object for
+    JSON serialization.
+    """
+    return vars(obj)
+

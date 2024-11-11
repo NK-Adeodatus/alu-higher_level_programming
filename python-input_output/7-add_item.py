e79b953321fca76eb3cf92ce6bf33face5c4ce54
NK-Adeodatus <a.nkundiman@alustudent.com>
2024-11-11 03:43:52 -0800

    added 7 file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,16 +3,9 @@
 
 
 import sys
-"""Imported sys"""
-
 from os.path import exists
-"""Imported exists"""
-
 from 5-save_to_json_file import save_to_json_file
-"""Imported save to json"""
-
 from 6-load_from_json_file import load_from_json_file
-"""Imported load from json"""
 
 
 filename = "add_item.json"

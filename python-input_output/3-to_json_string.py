77f04d8f598a90bb632ce65ee67011f8077934ca
NK-Adeodatus <a.nkundiman@alustudent.com>
2024-11-11 02:21:11 -0800

    added file 3-read_file.py

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Returning the json representation of an object"""
+
+
+def to_json_string(my_obj):
+    """Turning an object in a json string"""
+    return json.dumps(my_obj)

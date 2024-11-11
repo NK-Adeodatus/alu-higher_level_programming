69a8048acd60eac99d88c7c1ed8480423b2cf2a4
NK-Adeodatus <a.nkundiman@alustudent.com>
2024-11-11 03:39:44 -0800

    added 7 file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-"""
-Adding all arguments to a Python list, and then save
-them to a file
-"""
+"""Adding all arguments to a Python list, and then savethem to a file"""
 
 
 import sys
-
+"""Imported sys"""
 
 from os.path import exists
+"""Imported exists"""
+
 from 5-save_to_json_file import save_to_json_file
+"""Imported save to json"""
+
 from 6-load_from_json_file import load_from_json_file
+"""Imported load from json"""
 
 
 filename = "add_item.json"

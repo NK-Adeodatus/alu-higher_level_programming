d3a367c4fc41933dccdce11b4bc85946bee8eab4
NK-Adeodatus <a.nkundiman@alustudent.com>
2024-11-11 10:23:55 -0800

    added file 6-10

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- Script to list records from second_table ordered by score
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
---
+++ SQL_introduction/6-list_values.sql
@@ -0,0 +1,2 @@
+-- Script to list all rowsfrom the table first_table
+SELECT * FROM first_table

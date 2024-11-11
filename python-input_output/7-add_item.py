f476b9b08d5413390b4a4b476bf7380a7056511a
NK-Adeodatus <a.nkundiman@alustudent.com>
2024-11-11 03:48:02 -0800

    added 7 file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-"""Adding all arguments to a Python list, and then savethem to a file"""
-
+"""
+A script that adds all command-line arguments to a list and saves them 
+to a file named `add_item.json`. If the file doesn't exist, it creates a new one.
+Each time the script is run, new arguments are appended to the existing list in the file.
+"""
 
 import sys
 from os.path import exists
@@ -10,10 +13,15 @@ from 6-load_from_json_file import load_from_json_file
 
 filename = "add_item.json"
 
+# Check if the file exists, load or create the list accordingly
 if exists(filename):
     items = load_from_json_file(filename)
 else:
-    item =[]
+    items = []
 
-item.extend(sys.argv[1:])
+# Extend the list with new arguments from the command line
+items.extend(sys.argv[1:])
+
+# Save the updated list to the file
 save_to_json_file(items, filename)
+

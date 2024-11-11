cc07f41e996d881c54ae0ab1d39969e03664abcf
NK-Adeodatus <a.nkundiman@alustudent.com>
2024-11-11 03:17:15 -0800

    added file 7-read_file.py

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+adding all arguments to a Python list, and then save
+them to a file
+"""
+
+
+import sys
+from os.path import exists
+from 5-save_to_json_file import save_to_json_file
+from 6-load_from_json_file import load_from_json_file
+
+
+filename = "add_item.json"
+
+if exists(filename):
+    items = load_from_json_file(filename)
+else:
+    item =[]
+
+item.extend(sys.argv[1:])
+save_to_json_file(items, filename)

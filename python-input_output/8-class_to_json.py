8d1d8559a2ce9c8156dff6753f7b71d285f7f846
NK-Adeodatus <a.nkundiman@alustudent.com>
2024-11-11 03:58:42 -0800

    added file 8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -5,10 +5,10 @@ which takes an object
 and returns its dictionary representation for JSON serialization.
 """
 
+
 def class_to_json(obj):
     """
     Returns the dictionary description of an object for
     JSON serialization.
     """
     return vars(obj)
-

df7208b212180be2de135bd0db1c4ed65092da0a
NK-Adeodatus <a.nkundiman@alustudent.com>
2024-11-11 22:10:54 -0800

    added file 0-privilages

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,7 @@
+-- List all privilages of user_0d_1 and user_0d_2 on local host
+
+-- Listing privilages for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Listing privilages for user_0d2_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';

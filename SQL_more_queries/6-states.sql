598f689d2a3a8b76c527d5e378affe80bb271b3e
NK-Adeodatus <a.nkundiman@alustudent.com>
2024-11-13 08:24:39 -0800

    updates

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,9 +1,19 @@
--- Creating database hbtn_0d_usa and table states
+-- Create the database if it does not exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
+-- Switch to the database
 USE hbtn_0d_usa;
 
+-- Create the states table if it doesn't exist
 CREATE TABLE IF NOT EXISTS states (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(256) NOT NULL
-);	
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
+
+-- Create the cities table with a foreign key constraint
+CREATE TABLE IF NOT EXISTS cities (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id) ON DELETE CASCADE
+);

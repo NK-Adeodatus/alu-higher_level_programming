fc8e2f73c3783b06767329cc47f359e9c5164009
NK-Adeodatus <a.nkundiman@alustudent.com>
2024-11-18 03:48:32 -0800

    changes

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response.
-curl -s -H "X-HolbertonSchool-User-Id=98" "$1"
+# Script that takes in a URL as an argument, sends a GET request with a custom header, and displays the body of the response.
+curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"

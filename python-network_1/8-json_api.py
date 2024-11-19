2ed9e5d0955e3aaa3505b772d51daa2c2a215e94
NK-Adeodatus <a.nkundiman@alustudent.com>
2024-11-19 12:38:23 +0200

    imported reqests and sys

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -6,6 +6,9 @@ the letter as a parameter.
 """
 
 
+import requests
+import sys
+
 if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
     letter = sys.argv[1] if len(sys.argv) > 1 else ""

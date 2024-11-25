2fa9d8c88ce9676f8c5daab35d2f9c2a20a81de2
NK-Adeodatus <a.nkundiman@alustudent.com>
2024-11-25 19:06:20 +0200

    New updates in 0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python3
-"""Script that fetches https://intranet.hbtn.io/status."""
+"""
+This script fetches https://alu-intranet.hbtn.io/status and http://0.0.0.0:5050/status.
+"""
+
 import urllib.request
 
+with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as response:
+    body = response.read()
 
-if __name__ == "__main__":
-    request = urllib.request.Request("https://intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode('utf-8')))
+
+with urllib.request.urlopen('http://0.0.0.0:5050/status') as response:
+    body = response.read()
+
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode('utf-8')))

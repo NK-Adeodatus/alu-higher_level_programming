2f14f7740a0cea21d29371ddcdad63396cc630d6
NK-Adeodatus <a.nkundiman@alustudent.com>
2024-11-25 08:51:47 -0800

    add changes to file 0hbtn status

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -7,15 +7,6 @@ Module to add two integers
 def add_integer(a, b=98):
     """
         Add two integers or floats.
-
-        >>> add_integer(1, 2)
-        3
-        >>> add_integer(100.3, -2)
-        98
-        >>> add_integer("hello", 5)
-        Traceback (most recent call last):
-            ...
-        TypeError: a must be an integer
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
@@ -23,7 +14,3 @@ def add_integer(a, b=98):
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testmod()

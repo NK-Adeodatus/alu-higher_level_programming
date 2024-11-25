b92e5a1da863c396124239b87c5e00d1298cc58c
NK-Adeodatus <a.nkundiman@alustudent.com>
2024-11-25 19:46:40 +0200

    New updates in documentation of 7-error_code.py

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""
-Script that takes in a URL, sends a request
+"""script that takes in a URL, sends a request
 to the URL and displays the body of the response.
 """
-import requests
 import sys
+import requests
 
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    response = requests.get(url)
-    if response.status_code >= 400:
-        print(f"Error code: {response.status_code}")
+
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
     else:
-        print(response.text)
+        print(r.text)

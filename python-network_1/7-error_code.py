50928d6b4cbe2b59fe8873f113083f6add2e751a
NK-Adeodatus <a.nkundiman@alustudent.com>
2024-11-25 19:43:55 +0200

    Updates in the documentation of 7-error_code.py

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -3,10 +3,9 @@
 Script that takes in a URL, sends a request
 to the URL and displays the body of the response.
 """
-
-
 import requests
 import sys
+
 
 if __name__ == "__main__":
     url = sys.argv[1]

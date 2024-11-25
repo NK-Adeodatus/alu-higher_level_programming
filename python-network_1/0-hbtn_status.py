88437d023e2386300bfecd07fe1876d4664078f4
NK-Adeodatus <a.nkundiman@alustudent.com>
2024-11-25 08:59:39 -0800

    add changes to file 0hbtn status

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -6,19 +6,19 @@
 
 import urllib.request
 
-if __name__ == "__main__":
-    url = "http://0.0.0.0:5050/status"
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode('utf-8')))
 
-    with urllib.request.urlopen('http://0.0.0.0:5050/status') as response:
-        body = response.read()
+url = "https://alu-intranet.hbtn.io/status"
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode('utf-8')))
 
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode('utf-8')))
+with urllib.request.urlopen('http://0.0.0.0:5050/status') as response:
+    body = response.read()
+
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode('utf-8')))

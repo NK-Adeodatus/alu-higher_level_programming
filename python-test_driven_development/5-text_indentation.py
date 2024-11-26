9f6b90202d966e47e74aeaed2618a5a8c7fd9b68
NK-Adeodatus <a.nkundiman@alustudent.com>
2024-11-26 01:18:49 -0800

    updates

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -9,12 +9,22 @@ def text_indentation(text):
     """Prints a text with 2 new lines after each of
     these characters: ., ?, :
     """
-    if not isinstance(text, str):
+
+    if type(text) is not str:
         raise TypeError("text must be a string")
 
-    result = ""
-        for char in text:
-            result += char
-            if char in ".?:":
-                result = result.rstrip() + "\n\n"
-        print(result.strip(), end="")
+     no_space = True
+     size = 0
+     text = text.strip()
+     new_text = ""
+     for x in text:
+         if x == " " and no_space:
+             pass
+         elif x == "." or x == "?" or x == ":":
+             new_text += x + "\n\n"
+             no_space = True
+         else:
+             new_text += x
+             no_space = False
+     print(new_text, end='')
+

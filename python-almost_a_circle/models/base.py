3d256d00ac9f60aa85c6018a5f6fc13b1c802d80
NK-Adeodatus <a.nkundiman@alustudent.com>
2024-11-26 23:26:51 -0800

    complete task 1

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3
-"""Module containing the Base class."""
+"""Defines a base class."""
+import json
+import csv
+import turtle
 
 
 class Base:
-    """Base class for all future classes in this project."""
+    """Represent the base model.
+
+    Attributes:
+        __nb_objects (int): The number of instantiated Bases.
+    """
 
     __nb_objects = 0
 
     def __init__(self, id=None):
-    """Initialize a new Base instance
-    """
-    if id is not None:
-        self.id = id
-    else:
-        Base.__nb_objects += 1
-        self.id = Base.__nb_objects
+        """Initialize new Base.
 
+        Args:
+            id (int): The identity of the new Base.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects

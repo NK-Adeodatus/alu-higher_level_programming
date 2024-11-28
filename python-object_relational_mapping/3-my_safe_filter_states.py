624fba8c78081dc57ba5ea79007506ab4836c97b
NK-Adeodatus <a.nkundiman@alustudent.com>
2024-11-28 03:55:35 -0800

    add file 3 my safe filter

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
-"""Displays all values where name matches argument"""
+"""Displays all values in the states table of the database hbtn_0e_0_usa
+whose name matches that supplied as argument.
+Safe from SQL injections.
+Usage: ./3-my_safe_filter_states.py <mysql username> \
+                                    <mysql password> \
+                                    <database name> \
+                                    <state name searched>"""
 
-import MySQLdb
 import sys
+import MySQLdb
 
 if __name__ == "__main__":
     # Retrieve arguments passed to the script

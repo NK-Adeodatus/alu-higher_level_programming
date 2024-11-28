601a46b34c903d1381a50b17e58f4fcddb9a7437
NK-Adeodatus <a.nkundiman@alustudent.com>
2024-11-28 09:29:58 -0800

    updates in file 14

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -8,6 +8,7 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 from model_city import City
 
+
 def fetch_cities():
     if len(sys.argv) != 4:
         print("Usage: ./14-model_city_fetch_by_state.py " +
@@ -26,12 +27,18 @@ def fetch_cities():
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    cities = session.query(City, State).join(State).order_by(City.id).all()
+    cities = (
+        session.query(City, State)
+        .join(State)
+        .order_by(City.id)
+        .all()
+    )
 
     for city, state in cities:
         print(f"{state.name}: ({city.id}) {city.name}")
 
     session.close()
+
 
 if __name__ == '__main__':
     fetch_cities()

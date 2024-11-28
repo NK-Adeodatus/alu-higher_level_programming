23aa7836024c78d3bb4030ebdc58b644fe62902d
NK-Adeodatus <a.nkundiman@alustudent.com>
2024-11-28 09:48:43 -0800

    updates in file 14

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,44 +1,31 @@
 #!/usr/bin/python3
+
 """
-Prints all City objects from the database hbtn_0e_14_usa
+Fetches and displays all City objects from the database hbtn_0e_14_usa.
+Usage: ./14-model_city_fetch_by_state.py <mysql username> <mysql password> <database name>
 """
+
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+from model_state import State
 from model_city import City
 
-
-def fetch_cities():
-    if len(sys.argv) != 4:
-        print("Usage: ./14-model_city_fetch_by_state.py " +
-              "<mysql username> <mysql password> <database name>")
-        return
-
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-
-    engine = create_engine(
-        f'mysql+mysqldb://{mysql_username}:{mysql_password}'
-        f'@localhost/{database_name}'
+if __name__ == "__main__":
+    db_engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True,
     )
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    Session = sessionmaker(bind=db_engine)
+    db_session = Session()
 
-    cities = (
-        session.query(City, State)
-        .join(State)
+    results = (
+        db_session.query(City, State)
+        .join(State, City.state_id == State.id)
         .order_by(City.id)
-        .all()
     )
-
-    for city, state in cities:
+    for city, state in results:
         print(f"{state.name}: ({city.id}) {city.name}")
-
-    session.close()
-
-
-if __name__ == '__main__':
-    fetch_cities()

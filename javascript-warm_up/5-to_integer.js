735b55cf38c025e2d1ef041dcdc7b08e822cad53
NK-Adeodatus <a.nkundiman@alustudent.com>
2025-01-23 18:33:13 +0200

    Update 5-to_integer.js

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-let converted = parseInt(process.argv[2]);
-if (!isNaN(converted)){
+const converted = parseInt(process.argv[2]);
+if (!isNaN(converted)) {
   console.log(`My number: ${converted}`);
 } else {
   console.log('Not a number');

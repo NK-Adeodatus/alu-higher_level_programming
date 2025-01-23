adb66a4a83e3d59ca8f52c789c61585ca28d3945
NK-Adeodatus <a.nkundiman@alustudent.com>
2025-01-23 18:44:34 +0200

    Update 6-multi_languages_loop.js

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing']
-let i = 0
-while(i < 3){
-  console.log(arr[i])
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i = 0;
+while(i < 3) {
+  console.log(arr[i]);
 }
